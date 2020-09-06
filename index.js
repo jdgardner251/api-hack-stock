b0f7d0d9d7ada27606f2736a95836c3d2e2bad73
JD Gardner <jdgardner@aptuscapitaladvisors.com>
2020-09-06 17:40:06 -0500

    cleaning up code and adding design features

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 //Stock news API = 7odsz0k5jiqtfw0onngjimmcovsocjagkoasakw0
 //Stock news Endpoing = https://stocknewsapi.com/api/v1?tickers=FB&items=10&token=7odsz0k5jiqtfw0onngjimmcovsocjagkoasakw0
-
-// FB is the ticker above to add into the url
-
-const yahooURL = "https://yahoo-finance-free.p.rapidapi.com/v6/finance/quote?region=US&lang=en&symbols="
-const apiKey = "a34d8976d7mshb593b216380e63dp1ef646jsn7d7b2bf5b066";
 // header info:
 // x-rapidapi-host: yahoo-finance-free.p.rapidapi.com
 // x-rapidapi-key: a34d8976d7mshb593b216380e63dp1ef646jsn7d7b2bf5b066
 'use strict';
+/*****************               GLOBAL VARIABLES                ********************/
 
-// Take the object response from stockNews to reder the url, title, and image of articles
+const yahooURL = "https://yahoo-finance-free.p.rapidapi.com/v6/finance/quote?region=US&lang=en&symbols="
+const apiKey = "a34d8976d7mshb593b216380e63dp1ef646jsn7d7b2bf5b066";
+
+/***********  WATCHFORM TO WAIT FOR A SUBMIT   ************/
+function watchForm() {
+    //Listen for a submit on the button
+    $('form').submit(event => {
+      event.preventDefault();
+      const ticker = $('#js-ticker').val();
+
+      getNews(ticker);
+      getQuote(ticker);
+    });
+  }
+
+/***********         NEWS SECTION - TWO FUNCTIONS RELATING TO THE STOCK NEWS API TO RENDER LATEST ARTICLES        ************/
+
+// Pass object response from stockNews to reder the url, title, and image of articles
 function displayNews(responseJson){
-    console.log(responseJson)
-    console.log(responseJson.data[0].title)
     $('#results-list').empty();
     $('#js-error-message').empty();
     //loop throur news articles and show the title and description
@@ -23,33 +34,32 @@ function displayNews(responseJson){
         
         $('#results-list').append(
             `<li>
-                <h3><a href="${currentItem.news_url}">${currentItem.title}</a></h3>
+                <h3><a href="${currentItem.news_url}" target="_blank">${currentItem.title}</a></h3>
                 <img src="${currentItem.image_url}">
             </li>
             `
         )
     }
-
     $('#results').removeClass('hidden');
-    
 }
 // use the url to take the user input and fetch data from stocknews API
 function getNews(ticker) {
     const urlTicker = ticker;
-    console.log(urlTicker);
     const url = `https://stocknewsapi.com/api/v1?tickers=${urlTicker}&items=4&token=7odsz0k5jiqtfw0onngjimmcovsocjagkoasakw0`
 
     fetch(url)
         .then(response => response.json()
         .then(responseJson => displayNews(responseJson))
         .catch(err => {
-            $('#js-error-message').text(`something went wrong: ${err.message}`);
+            $('#js-error-message').text(`We don't recognize that ticker. Please try another`);
+            $('#results-list').empty();
         }));
 }
 
+/***********  QUOTE SECTION - TWO FUNCTIONS RELATING TO THE YAHOO API TO RENDER QUOTE INFORMATION  ************/
+
 // take the response object to show the symbol, current price, and %Change from yesterday
 function displayQuote(responseJson){
-    console.log(responseJson);
     const currentStock = responseJson.quoteResponse.result[0];
     $('#quote').empty();
     $('#js-error-message').empty();
@@ -61,19 +71,14 @@ function displayQuote(responseJson){
                 <div class="item">Current Price: ${currentStock.regularMarketPrice} </div>
                 <div class="item">% Change: ${currentStock.regularMarketChangePercent.toFixed(2)}</div>
             </div>
-           
             <hr>
         `
     )
-
     $('#quote').removeClass('hidden');
-
 }
  // use yahoo finance API to take the user input combined with yahoo API url to request data
 function getQuote(ticker) {
-    console.log('Get quote is running')
     const queryURL = yahooURL + ticker;
-    console.log(queryURL);
 // this are the required headers to be passed in with the url to the fetch
     const options = {
         headers: new Headers({
@@ -86,22 +91,9 @@ function getQuote(ticker) {
         .then(response => response.json()
         .then(responseJson => displayQuote(responseJson))
         .catch(err => {
-            $('#js-error-message').text(`something went wrong: ${err.message}`);
+            $('#js-error-message').text(`We don't recognize that ticker. Please try another`);
+            $('#results-list').empty();
         }))
-
 }
-
-function watchForm() {
-    //Listen for a submit on the button
-    $('form').submit(event => {
-      event.preventDefault();
-      const ticker = $('#js-ticker').val();
-      console.log(ticker);
-
-      getNews(ticker);
-      getQuote(ticker);
-  
-    });
-  }
 
   $(watchForm);
