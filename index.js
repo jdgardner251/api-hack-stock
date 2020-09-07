b7ee8f4d4c0c179e63f71159eeefd920c09d2b57
JD Gardner <jdgardner@aptuscapitaladvisors.com>
2020-09-07 09:50:30 -0500

    fixed error messages and accessibility

--- index.js
+++ index.js
@@ -26,7 +26,11 @@ function watchForm() {
 // Pass object response from stockNews to reder the url, title, and image of articles
 function displayNews(responseJson){
     $('#results-list').empty();
-    $('#js-error-message').empty();
+    if (responseJson.data.length === 0){
+        $('#js-news-error-message').text(`We can't find any news for this ticker!`);
+    } else {
+        
+    $('#js-news-error-message').empty();
     //loop throur news articles and show the title and description
     // provide title as a hyperlink
     for (let i = 0; i < responseJson.data.length; i++){
@@ -41,6 +45,11 @@ function displayNews(responseJson){
         )
     }
     $('#results').removeClass('hidden');
+
+    }
+
+
+    
 }
 // use the url to take the user input and fetch data from stocknews API
 function getNews(ticker) {
@@ -48,33 +57,38 @@ function getNews(ticker) {
     const url = `https://stocknewsapi.com/api/v1?tickers=${urlTicker}&items=4&token=7odsz0k5jiqtfw0onngjimmcovsocjagkoasakw0`
 
     fetch(url)
-        .then(response => response.json()
+        .then(response => response.json())
         .then(responseJson => displayNews(responseJson))
         .catch(err => {
-            $('#js-error-message').text(`We don't recognize that ticker. Please try another`);
+            $('#js-news-error-message').text(`Something went wrong.`);
             $('#results-list').empty();
-        }));
+        });
 }
 
 /***********  QUOTE SECTION - TWO FUNCTIONS RELATING TO THE YAHOO API TO RENDER QUOTE INFORMATION  ************/
 
 // take the response object to show the symbol, current price, and %Change from yesterday
 function displayQuote(responseJson){
-    const currentStock = responseJson.quoteResponse.result[0];
     $('#quote').empty();
-    $('#js-error-message').empty();
-    $('#quote').html(
-        `<h3 class="long-name">${currentStock.longName}</h3>
-            <hr>
-            <div class="group">
-                <div class="item">Ticker: ${currentStock.symbol}</div>
-                <div class="item">Current Price: ${currentStock.regularMarketPrice} </div>
-                <div class="item">% Change: ${currentStock.regularMarketChangePercent.toFixed(2)}</div>
-            </div>
-            <hr>
-        `
-    )
-    $('#quote').removeClass('hidden');
+    if (responseJson.quoteResponse.result.length === 0){
+        $('#js-quote-error-message').text(`We don't recognize that ticker. Please try another`);
+    } else {
+        const currentStock = responseJson.quoteResponse.result[0];
+        $('#js-quote-error-message').empty();
+        $('#quote').html(
+            `<h3 class="long-name">${currentStock.longName}</h3>
+                <hr>
+                <div class="group">
+                    <div class="item">Ticker: ${currentStock.symbol}</div>
+                    <div class="item">Current Price: ${currentStock.regularMarketPrice} </div>
+                    <div class="item">% Change: ${currentStock.regularMarketChangePercent.toFixed(2)}</div>
+                </div>
+                <hr>
+            `
+        )
+        $('#quote').removeClass('hidden');
+    }
+ 
 }
  // use yahoo finance API to take the user input combined with yahoo API url to request data
 function getQuote(ticker) {
@@ -88,12 +102,13 @@ function getQuote(ticker) {
     };
 
     fetch(queryURL, options)
-        .then(response => response.json()
+        .then(response => response.json())
         .then(responseJson => displayQuote(responseJson))
         .catch(err => {
-            $('#js-error-message').text(`We don't recognize that ticker. Please try another`);
+            console.log(err);
+            $('#js-quote-error-message').text(`We don't recognize that ticker. Please try another`);
             $('#results-list').empty();
-        }))
+        })
 }
 
   $(watchForm);
